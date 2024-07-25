e326532e0e218bfb741f580de1b306f974fcff4d
SonSuBin129 <ttokky0129@gmail.com>
2024-07-26 01:05:56 +0900

    chore: 불필요한 Import 제거

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,11 @@
 import Nav from "@/components/Nav";
 import { Toaster } from "@ui/components/ui/toaster";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
-  const { pathname } = useLocation();
-
   return (
     <section className="relative flex h-dvh w-screen flex-col">
       <Toaster className="top-5" isDestructive />
-
       <header className="sticky left-0 top-0 z-10 bg-white">
         <Nav />
       </header>
