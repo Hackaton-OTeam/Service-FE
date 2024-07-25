a189e93915a80446c9664f0894fad7692837e4f2
SonSuBin129 <ttokky0129@gmail.com>
2024-07-26 02:02:56 +0900

    style: Nav바 수정

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,29 +1,40 @@
 import { Link, useLocation } from "react-router-dom";
-
 import usePreviousPath from "@/hooks/usePreviousPath";
-
 import Logo from "@/components/Logo";
 import BackIcon from "@/components/BackIcon";
+import { cn } from "@ui/lib/utils";
 
 const Nav = () => {
   const { pathname } = useLocation();
   const [previousPath, popPreviousPath] = usePreviousPath();
 
-  const noNav = ["/", "/login"];
-  if (noNav.includes(pathname)) return null;
+  const noNavPaths = ["/", "/login"];
+  const showLogo = pathname === "/home";
+  const showNav = !noNavPaths.includes(pathname);
+  const NavTitle = "회원가입";
 
-  const showLogo = ["/home"].includes(pathname);
+  if (!showNav) return null;
 
   return (
-    <nav className="my-2 flex h-10 w-full justify-between self-stretch px-4">
+    <nav
+      className={cn(
+        "my-2 flex h-10 w-full self-stretch px-4",
+        showLogo ? "justify-between" : "justify-start",
+      )}
+    >
       {showLogo ? (
-        <Logo size={"10%"} isNav />
+        <Logo size="10%" isNav />
       ) : (
-        <div className="flex items-center">
-          <Link to={previousPath} onClick={popPreviousPath}>
-            <BackIcon />
-          </Link>
-        </div>
+        <>
+          <div className="flex items-center">
+            <Link to={previousPath} onClick={popPreviousPath}>
+              <BackIcon />
+            </Link>
+          </div>
+          <div className="grow text-center text-2xl font-bold leading-10">
+            {NavTitle}
+          </div>
+        </>
       )}
     </nav>
   );
