fb51145120a6d38d12f2088d4aa5efb6b830ed81
SonSuBin129 <ttokky0129@gmail.com>
2024-07-26 01:05:15 +0900

    feat: 백 네비 기능 수정

---
+++ src/components/Nav.tsx
@@ -0,0 +1,29 @@
+import { Link, useLocation } from "react-router-dom";
+
+import usePreviousPath from "@/hooks/usePreviousPath";
+
+import Logo from "@/components/Logo";
+
+const Nav = () => {
+  const { pathname } = useLocation();
+  const [previousPath, popPreviousPath] = usePreviousPath();
+
+  const noNav = ["/", "/login"];
+  if (noNav.includes(pathname)) return null;
+
+  const showLogo = ["/home"].includes(pathname);
+
+  return (
+    <nav className="my-2 flex h-10 w-full justify-between self-stretch px-4">
+      {showLogo ? (
+        <Logo size={"10%"} />
+      ) : (
+        <Link to={previousPath} onClick={popPreviousPath}>
+          ddd
+        </Link>
+      )}
+    </nav>
+  );
+};
+
+export default Nav;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,23 @@
+import Nav from "@/components/Nav";
 import { Toaster } from "@ui/components/ui/toaster";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 const App = () => {
+  const { pathname } = useLocation();
+
   return (
-    <>
+    <section className="relative flex h-dvh w-screen flex-col">
       <Toaster className="top-5" isDestructive />
 
+      <header className="sticky left-0 top-0 z-10 bg-white">
+        <Nav />
+      </header>
       <section className="relative flex h-dvh w-screen flex-col">
         <main className="w-full grow">
           <Outlet />
         </main>
       </section>
-    </>
+    </section>
   );
 };
 
