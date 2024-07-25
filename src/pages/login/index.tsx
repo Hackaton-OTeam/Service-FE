fb093b267ce58bdaba3375eef8f029795548b681
SonSuBin129 <ttokky0129@gmail.com>
2024-07-25 22:09:25 +0900

    style: toast 조건문 추가

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,17 +1,48 @@
 import Logo from "@/components/Logo";
 import { MailInputField } from "@/pages/login/components/MailInputField";
 import { PWInputField } from "@/pages/login/components/PWInputField";
-import { Link } from "@/router";
+import { Link, useNavigate } from "@/router";
 import { Button } from "@ui/components/ui/button";
 import { useToast } from "@ui/components/ui/use-toast";
+import { ChangeEvent, useState } from "react";
 
 const Login = () => {
-  //TODO: 백엔드 api 연결해서 계정이 없는 경우 에러가 발생하게 함.
-
   const { toast } = useToast();
+  const navigate = useNavigate();
+
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setValues(prevValues => {
+      const { name, value } = event.target;
+      return {
+        ...prevValues,
+        [name]: value,
+      };
+    });
+  };
+
+  const [loginFalse, setLoginFalse] = useState(false);
+  let toastContent = "";
   const handleLogin = () => {
+    if (values.email === "") {
+      toastContent = "이메일을 입력해주세요.";
+    } else if (values.password === "") {
+      toastContent = "비밀번호를 입력해주세요.";
+    } else {
+      // 백엔드 로그인 API 연결
+      if (loginFalse) toastContent = "이메일 또는 비밀번호를 확인해주세요.";
+      else {
+        navigate("/home", { replace: true });
+        return;
+      }
+    }
+
     toast({
-      title: "이메일 또는 비밀번호를 확인해주세요.",
+      title: toastContent,
       variant: "brandDestructive",
     });
   };
@@ -23,8 +54,18 @@ const Login = () => {
         <div>서비스명 이미지로 들어올 것</div>
       </section>
       <main className="flex w-full flex-col gap-5">
-        <MailInputField placeholder="이메일을 입력해주세요." />
-        <PWInputField placeholder="비밀번호를 입력해주세요." />
+        <MailInputField
+          placeholder="이메일을 입력해주세요."
+          name="email"
+          value={values.email}
+          onChange={handleChange}
+        />
+        <PWInputField
+          placeholder="비밀번호를 입력해주세요."
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+        />
       </main>
       <footer className="flex w-full flex-col items-center gap-4">
         <Button
