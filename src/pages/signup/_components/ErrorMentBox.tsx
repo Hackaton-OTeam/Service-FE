e24dbe60a1ef80321843f1d445359f73475f16ae
SonSuBin129 <ttokky0129@gmail.com>
2024-07-27 01:51:35 +0900

    style: error멘트 박스 수정

--- src/pages/signup/_components/ErrorMentBox.tsx
+++ src/pages/signup/_components/ErrorMentBox.tsx
@@ -2,18 +2,21 @@ import { cn } from "@ui/lib/utils";
 
 interface ErrorMentBoxProps {
   isPass?: boolean;
+  ment: string;
 }
 
 const ErrorMentBox = (props: ErrorMentBoxProps) => {
-  const { isPass } = props;
+  const { isPass, ment } = props;
 
   return (
     <div
-      className={cn("flex items-center text-[13px]", {
+      className={cn("flex items-center px-3 text-[13px]", {
         "text-brand": isPass,
         "text-brandError": !isPass,
       })}
-    ></div>
+    >
+      {ment}
+    </div>
   );
 };
 
