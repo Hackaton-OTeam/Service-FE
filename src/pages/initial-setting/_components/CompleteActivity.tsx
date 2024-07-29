164a460968461c0e1f15839754cbbf83a3f9a273
SonSuBin129 <ttokky0129@gmail.com>
2024-07-29 21:30:24 +0900

    chore: 패키지 제거

--- src/pages/initial-setting/_components/CompleteActivity.tsx
+++ src/pages/initial-setting/_components/CompleteActivity.tsx
@@ -9,6 +9,7 @@ import {
 
 import { AppScreen } from "@stackflow/plugin-basic-ui";
 import NextStepButton from "./NextStepButton";
+import BackIcon from "@/components/BackIcon";
 
 interface CompleteParams extends ActivityParams {}
 
@@ -18,7 +19,16 @@ const CompleteActivity: ActivityComponentType<CompleteParams> = ({
   const { form } = params;
 
   return (
-    <AppScreen appBar={{ border: false }}>
+    <AppScreen
+      appBar={{
+        border: false,
+        height: "56px",
+        backButton: {
+          renderIcon: () => <BackIcon />,
+          ariaLabel: "Go Back",
+        },
+      }}
+    >
       <Activity>
         <ActivityContent>
           <section className="grow">
