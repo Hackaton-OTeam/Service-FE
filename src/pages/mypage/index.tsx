33a6258eaefd9292e41d65e14f461ce32805f4b5
SonSuBin129 <ttokky0129@gmail.com>
2024-07-30 22:23:14 +0900

    style: 상단 네비바 추가

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,11 +1,11 @@
 import BottomNav from "@/components/BottomNav";
-import Nav from "@/components/Nav";
+import SubNav from "@/components/SubNav";
 
 const MyPage = () => {
   return (
     <main className="flex h-full flex-col">
       <header className="sticky left-0 top-0 z-10 bg-white py-2">
-        <Nav />
+        <SubNav NavTitle="마이페이지" />
       </header>
       <main className="flex h-full flex-col items-center justify-start gap-10 px-4">
         마이페이지
