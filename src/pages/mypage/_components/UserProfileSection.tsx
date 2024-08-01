455aa89ef271d66a3b3a4d52d085671e7f4afbd2
SonSuBin129 <ttokky0129@gmail.com>
2024-08-01 21:47:51 +0900

    feat: 로그아웃 api 연결 & 마이페이지 접근시 로직 추가

--- src/pages/mypage/_components/UserProfileSection.tsx
+++ src/pages/mypage/_components/UserProfileSection.tsx
@@ -4,22 +4,30 @@ import BlueCharacter from "@/components/character/BlueCharacter";
 
 import { useMutationMyPage } from "@/hooks/mutation/useMutationMyPage";
 
+import { useNavigate } from "@/router";
+
 const UserProfileSection = () => {
+  const navigate = useNavigate();
   const mutation = useMutationMyPage();
 
   const [nickname, setNickName] = useState("");
   const [userLevel, setUserLevel] = useState("");
 
   useEffect(() => {
-    mutation.mutate(
-      { userEmail: localStorage.getItem("userEmail")! },
-      {
-        onSuccess: data => {
-          setNickName(data.userName);
-          setUserLevel(data.level);
+    const userEmail = localStorage.getItem("userEmail");
+    if (!userEmail) {
+      navigate("/login", { replace: true });
+    } else {
+      mutation.mutate(
+        { userEmail: userEmail },
+        {
+          onSuccess: data => {
+            setNickName(data.userName);
+            setUserLevel(data.level);
+          },
         },
-      },
-    );
+      );
+    }
   }, []);
 
   return (
