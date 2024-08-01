0e0f2a4bc51b9d123c857860de5fcb5fbb75145d
SonSuBin129 <ttokky0129@gmail.com>
2024-08-01 21:24:27 +0900

    feat: 마이페이지 api 연결

--- src/pages/mypage/_components/UserProfileSection.tsx
+++ src/pages/mypage/_components/UserProfileSection.tsx
@@ -1,15 +1,36 @@
+import { useEffect, useState } from "react";
+
 import BlueCharacter from "@/components/character/BlueCharacter";
 
+import { useMutationMyPage } from "@/hooks/mutation/useMutationMyPage";
+
 const UserProfileSection = () => {
+  const mutation = useMutationMyPage();
+
+  const [nickname, setNickName] = useState("");
+  const [userLevel, setUserLevel] = useState("");
+
+  useEffect(() => {
+    mutation.mutate(
+      { userEmail: localStorage.getItem("userEmail")! },
+      {
+        onSuccess: data => {
+          setNickName(data.userName);
+          setUserLevel(data.level);
+        },
+      },
+    );
+  }, []);
+
   return (
     <div className="flex w-full flex-col items-center gap-4">
       <div className="flex aspect-square w-[28%] items-center justify-center rounded-full border border-[#c6c6c6]">
         <BlueCharacter size={"65%"} />
       </div>
       <div className="flex w-full flex-col gap-1 text-center">
-        <div className="text-2xl font-bold text-brand">[닉네임]</div>
+        <div className="text-2xl font-bold text-brand">[{nickname}]</div>
         <div className="text-base font-medium text-brandSubText">
-          LV. [레벨]
+          LV. {userLevel}
         </div>
       </div>
     </div>
