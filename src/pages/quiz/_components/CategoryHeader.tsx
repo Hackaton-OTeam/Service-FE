9f22bbcd7e0b1b83653aa0cf7de7a2e79ab61e2a
SonSuBin129 <ttokky0129@gmail.com>
2024-08-03 21:29:16 +0900

    style: 컴포넌트 위치 수정

--- src/pages/quiz/_components/CategoryHeader.tsx
+++ src/pages/quiz/_components/CategoryHeader.tsx
@@ -25,7 +25,7 @@ const CategoryHeader = (props: CategoryHeaderProps) => {
 
   return (
     <section
-      className="h-90 flex w-full flex-col justify-between px-4 pb-8 pt-5"
+      className="h-90 relative flex h-[330px] w-full flex-col justify-between px-4 pb-8 pt-5"
       style={{ backgroundColor }}
     >
       <div className="flex flex-col gap-[7px]">
@@ -40,7 +40,7 @@ const CategoryHeader = (props: CategoryHeaderProps) => {
       </div>
       <div className="flex flex-col gap-1">
         <div className="flex justify-between">
-          <div className="flex gap-1 font-extrabold">
+          <div className="absolute bottom-[37px] left-4 z-30 flex w-28 gap-1 font-extrabold">
             <h1 className="self-end text-[75px] leading-[75px] text-[#547AFF]">
               {learningLearned}
             </h1>
@@ -48,7 +48,9 @@ const CategoryHeader = (props: CategoryHeaderProps) => {
               /{learningTotal}개
             </p>
           </div>
-          <CategoryComponent />
+          <div className="absolute bottom-[50px] right-4">
+            <CategoryComponent />
+          </div>
         </div>
         <ProgressBar percent={learningRate} />
       </div>
