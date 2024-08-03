86e2962412e3cb74d18f9e9e957118ec6ac7686c
SonSuBin129 <ttokky0129@gmail.com>
2024-08-04 02:57:15 +0900

    feat: 단어 스크랩 api 초기세팅

---
+++ src/api/scrap.ts
@@ -0,0 +1,11 @@
+import { WordScrapParams } from "@/types/scrapType";
+
+import { instance } from "@/api/instance";
+
+export const scrapWord = async ({ userEmail, wordId }: WordScrapParams) => {
+  const { data } = await instance.post("/scrap/word-save", {
+    userEmail,
+    wordId,
+  });
+  return data;
+};
