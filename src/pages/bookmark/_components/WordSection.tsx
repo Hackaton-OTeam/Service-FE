4db9c0e42781285451190fa9384f11e9ffe72ea2
SonSuBin129 <ttokky0129@gmail.com>
2024-08-04 15:45:43 +0900

    feat: api 연결 초기세팅

--- src/pages/bookmark/_components/WordSection.tsx
+++ src/pages/bookmark/_components/WordSection.tsx
@@ -1,4 +1,9 @@
+import { useQueryScrapWord } from "@/hooks/queries/useQueryScrapWord";
+
 const WordSection = () => {
+  const userEmail = localStorage.getItem("userEmail");
+  const { data: scrapWords } = useQueryScrapWord(userEmail!);
+
   return <div>WordSection</div>;
 };
 export default WordSection;
