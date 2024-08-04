88463b990c74579796498450e5708cac8ff55e9e
SonSuBin129 <ttokky0129@gmail.com>
2024-08-05 03:04:43 +0900

    feat: 일반상식 api 연결

--- src/pages/home/_components/CommonSenseItem.tsx
+++ src/pages/home/_components/CommonSenseItem.tsx
@@ -4,10 +4,24 @@ import { useManageCommonSense } from "@/hooks/useManageCommonSense";
 
 interface CommonSenseItemProps {
   id: number;
+  date: string;
 }
 
 const CommonSenseItem = (props: CommonSenseItemProps) => {
-  const { id } = props;
+  const { id, date } = props;
+
+  // 현재 날짜를 YYYY-MM-DD 형식으로 변환하는 함수
+  const getCurrentDateString = () => {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, "0"); // 월은 0부터 시작하므로 1을 더해줍니다.
+    const day = String(now.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  };
+
+  // 오늘 날짜와 비교
+  const currentDateString = getCurrentDateString();
+  const displayDate = date === currentDateString ? "오늘" : date;
 
   const { image, title1, title2, explain, class1, class2, backgroundColor } =
     useManageCommonSense(id, "first");
@@ -22,31 +36,34 @@ const CommonSenseItem = (props: CommonSenseItemProps) => {
   };
 
   return (
-    <div
-      className="relative flex h-[347px] w-[292px] flex-col gap-4 rounded-[34px] px-4 pt-10 font-bold"
-      style={itemStyle}
-    >
-      <div className="z-20 flex flex-col gap-[10px]">
-        <h1 className="text-[27px] leading-tight">
-          {title1}
-          <br />
-          {title2}
-        </h1>
-        <p className="text-base text-[#8c8c8c]">{explain}</p>
+    <section className="flex flex-col gap-[5px]">
+      <div className="text-sm">{displayDate}</div>
+      <div
+        className="relative flex h-[347px] w-[292px] flex-col gap-4 rounded-[34px] px-4 pt-10 font-bold"
+        style={itemStyle}
+      >
+        <div className="z-20 flex flex-col gap-[10px]">
+          <h1 className="text-[27px] leading-tight">
+            {title1}
+            <br />
+            {title2}
+          </h1>
+          <p className="text-base text-[#8c8c8c]">{explain}</p>
+        </div>
+        <div className="z-20 flex gap-2">
+          <ClassComponent title={class1} />
+          <ClassComponent title={class2} />
+        </div>
+        <div className="absolute bottom-2 right-2 z-10">
+          <img
+            src={image}
+            alt="First Apple"
+            className="h-[202px] w-auto"
+            loading="lazy"
+          />
+        </div>
       </div>
-      <div className="z-20 flex gap-2">
-        <ClassComponent title={class1} />
-        <ClassComponent title={class2} />
-      </div>
-      <div className="absolute bottom-2 right-2 z-10">
-        <img
-          src={image}
-          alt="First Apple"
-          className="h-[202px] w-auto"
-          loading="lazy"
-        />
-      </div>
-    </div>
+    </section>
   );
 };
 
--- src/pages/home/_components/CommonSenseSection.tsx
+++ src/pages/home/_components/CommonSenseSection.tsx
@@ -4,17 +4,21 @@ import {
   CarouselItem,
 } from "@ui/components/ui/carousel";
 
+import { useQueryCommonList } from "@/hooks/queries/useQueryCommonList";
+
 import CommonSenseItem from "./CommonSenseItem";
 
 const CommonSenseSection = () => {
+  const { data: commonList } = useQueryCommonList();
+
   return (
     <section className="flex w-full flex-col gap-[10px]">
-      <div className="pl-px text-xl font-bold">카테고리별 어휘 학습</div>
+      <div className="pl-px text-xl font-bold">휘릭! 매일 어휘 한 조각</div>
       <Carousel className="w-full max-w-full">
         <CarouselContent className="-ml-4">
-          {[1, 2, 3, 4].map(id => (
+          {commonList.map(({ id, date }) => (
             <CarouselItem key={id} className="basis-5/6 pl-4">
-              <CommonSenseItem id={id} />
+              <CommonSenseItem id={id} date={date} />
             </CarouselItem>
           ))}
         </CarouselContent>
