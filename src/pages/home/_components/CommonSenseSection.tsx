e27b12b6d7718b604971944bc5c2afbf7103cb7b
SonSuBin129 <ttokky0129@gmail.com>
2024-08-05 02:44:49 +0900

    fix: 오타 수정

--- src/pages/home/_components/CommonSenseSection.tsx
+++ src/pages/home/_components/CommonSenseSection.tsx
@@ -11,7 +11,7 @@ const CommonSenseSection = () => {
     <section className="flex w-full flex-col gap-[10px]">
       <div className="pl-px text-xl font-bold">카테고리별 어휘 학습</div>
       <Carousel className="w-full max-w-full">
-        <CarouselContent className="R-ml-4">
+        <CarouselContent className="-ml-4">
           {[1, 2, 3, 4].map(id => (
             <CarouselItem key={id} className="basis-5/6 pl-4">
               <CommonSenseItem id={id} />
