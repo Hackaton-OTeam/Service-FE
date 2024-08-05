801088dc9ec32a532d22c0fd1e7f60de5aaabce7
SonSuBin129 <ttokky0129@gmail.com>
2024-08-05 21:07:58 +0900

    feat: 책갈피 완료

--- src/components/ClassComponent.tsx
+++ src/components/ClassComponent.tsx
@@ -6,7 +6,7 @@ const ClassComponent = (props: ClasComponentProps) => {
   const { title } = props;
 
   return (
-    <div className="flex w-fit items-center justify-center rounded-[36px] border border-brand bg-white px-2.5 py-[3px] text-[13px] font-semibold text-brand">
+    <div className="flex w-fit items-center justify-center rounded-[36px] border border-brand bg-white px-2.5 py-[1.5px] text-[13px] font-semibold text-brand">
       {title}
     </div>
   );
--- src/hooks/queries/useQueryScrapKnowledge.ts
+++ src/hooks/queries/useQueryScrapKnowledge.ts
@@ -2,7 +2,7 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 
 import { getScrapKnowledge } from "@/api/scrap";
 
-export const useQueryScrapWord = (userEmail: string) => {
+export const useQueryScrapKnowledge = (userEmail: string) => {
   const { data } = useSuspenseQuery({
     queryKey: ["scrap-knowledge-list", userEmail],
     queryFn: () => getScrapKnowledge(userEmail),
